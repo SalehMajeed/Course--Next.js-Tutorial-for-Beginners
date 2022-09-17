e7c57149c6d1c5cddafc2f6e8457dda644e8068d
salehmajeed <saleh.majeed8@gmail.com>
2022-09-17 09:48:50 +0530

    link

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,21 @@
-function ProductList() {
-    return (
-        <>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
-        </>
-    )
+import Link from "next/link";
+
+function ProductList({ initialPage = 100 }) {
+  return (
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+
+      <Link href={`product/1`} replace>
+        <a>Product 1</a>
+      </Link>
+
+      <Link href={`product/${initialPage}`}>
+        <a>Product 100</a>
+      </Link>
+    </>
+  );
 }
 
 export default ProductList;
