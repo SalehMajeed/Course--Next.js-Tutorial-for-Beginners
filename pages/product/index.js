cdcce6318494126c37d429f570a252a7b880d469
salehmajeed <saleh.majeed8@gmail.com>
2022-09-17 11:54:54 +0530

    navigator

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function ProductList({ initialPage = 100 }) {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("done");
+    router.push("/");
+  };
   return (
     <>
       <Link href="/">
@@ -14,6 +20,7 @@ function ProductList({ initialPage = 100 }) {
       <Link href={`product/${initialPage}`}>
         <a>Product 100</a>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </>
   );
 }
