53cbafca0d31858959b78e19228ebe4dbc693d39
salehmajeed <saleh.majeed8@gmail.com>
2022-09-29 15:03:19 +0530

    SASS

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
-import style from "../styles/About.module.css";
+// import style from "../styles/About.module.css";
+import style from "../styles/About.module.scss";
 function About() {
   return (
-    <button className={(style.highlight, "btn btn-primary")}>button</button>
+    // <button className={(style.highlight, "btn btn-primary")}>button</button>
+    <div className={style.highlightscss}>About</div>
   );
 }
 
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,6 @@
-import styles from "../styles/Contact.module.css";
+// import styles from "../styles/Contact.module.css";
+import styles from "../styles/Contact.module.scss";
 export default function contact() {
-  return <h2 className={styles.highlight}>contact Page</h2>;
+  // return <h2 className={styles.highlight}>contact Page</h2>;
+  <div className={styles.highlightscss}>Contact</div>;
 }
